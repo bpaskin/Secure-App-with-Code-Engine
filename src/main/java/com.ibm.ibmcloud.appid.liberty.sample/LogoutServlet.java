be109a7496c2996ee2c36c6d217f712e91e0ef27
Brian S Paskin <brian@paskino.com>
2024-06-12 14:16:14 -0400

    removed old uri

--- src/main/java/com.ibm.ibmcloud.appid.liberty.sample/LogoutServlet.java
+++ src/main/java/com.ibm.ibmcloud.appid.liberty.sample/LogoutServlet.java
@@ -14,6 +14,6 @@ public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.logout();
-        resp.sendRedirect("/appidSample");
+        resp.sendRedirect("/");
     }
 }
